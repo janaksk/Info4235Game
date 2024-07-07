868abfa8a66e71e00a09a8b181ccd2bd48296932
Janak Kalar <65189769+janaksk@users.noreply.github.com>
2024-07-07 12:31:51 -0700

    Adds Fade transitions between levels

--- scenes/Level2Scene.js
+++ scenes/Level2Scene.js
@@ -16,6 +16,11 @@ class Level2Scene extends Phaser.Scene {
     volMusic = 1;
     volSFX = .5;
 
+    init () {
+        // Camera Fade in
+        this.cameras.main.fadeIn(800);
+    }
+
     preload() {
         // Background Assets
         this.load.image('sky', 'assets/backgrounds/sky.png');
@@ -35,7 +40,6 @@ class Level2Scene extends Phaser.Scene {
     }
 
     create() {
-
         // Putting Foreground and midground
         this.add.image(400, 300, 'sky');
         this.add.image(400, 300, 'midground');
@@ -96,7 +100,13 @@ class Level2Scene extends Phaser.Scene {
         this.scoreText.setText(`Score: ${this.score} /10`);
 
         if (this.score === 10) {
-            this.scene.start('Level3Scene');
+            // Fade Out Camera
+            this.cameras.main.fadeOut(800);
+
+            // Start next level after camera has faded
+            this.time.delayedCall(800, function() {
+                this.scene.start('Level3Scene');
+            }, [], this);
         }
     }
 }
